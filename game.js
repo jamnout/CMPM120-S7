38c92a57a0f3aae1425520328e0b5646c2d276a5
jamnout <topadam7@gmail.com>
2026-05-20 18:18:49 -0700

    added enemy sprite and movement

--- game.js
+++ game.js
@@ -6,11 +6,15 @@ class GameScene extends Phaser.Scene {
     }
     create() {
         this.add.graphics();
-        this.Title = this.add.text(
-            115,
-            207,
-            "Hello",
-        );
+        this.enemy = this.add.rectangle(750, 500, 50, 100, 0x0000ff)
+
+        this.tweens.add({
+            targets: this.enemy,
+            duration: 3000,
+            x: 50,
+            ease: 'linear',
+            loop: -1
+        })
 
     }
 }
