c9e9bdf6347ca247d147f39599a4c28dfacadfd6
wsun0301 <wsun0301@gmail.com>
2026-05-20 18:26:37 -0700

    added player and player movement

--- game.js
+++ game.js
@@ -16,5 +16,21 @@ class GameScene extends Phaser.Scene {
             loop: -1
         })
 
+        this.player = this.add.circle(400, 525, 25, 0xff0000)
+
+        this.input.keyboard.on('keydown-SPACE', () => {
+
+                if (this.player.y === 525) {
+                    this.tweens.add({
+                        targets: this.player,
+                        duration: 500,
+                        y: 300,
+                        ease: 'Power2',
+                        yoyo: true
+                    })
+            }
+        })
+
+
     }
 }
